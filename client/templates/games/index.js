711003c0b0956a73ea32db3ffb51a1e11f9da214
Eric Yang <e@eric-y.com>
2015-07-09 13:39:50 -0500

    Added show template

--- client/templates/games/index.js
+++ client/templates/games/index.js
@@ -3,11 +3,18 @@ Template.gamesList.helpers({
     return Games.find();
   }
 });
-
 Template.gameItem.helpers({
   domain: function() {
     var a = document.createElement('a');
     a.href = this.url;
     return a.hostname;
+  }
+});
+Template.gameItem.events({
+  'click tr': function(e) {
+    e.preventDefault();
+
+    Router.go('gamePage', { _id: this._id });
+
   }
 });
