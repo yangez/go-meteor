1f98de6bbb078ac2d16730a69047d98e4110f524
MKS-students <voxlol@outlook.com>
2015-07-21 18:34:27 -0500

    before rebase commit

--- client/templates/accountMenu/accountMenu.js
+++ client/templates/accountMenu/accountMenu.js
@@ -2,4 +2,7 @@ Template._loginButtonsLoggedInDropdown.events({
   'click #login-buttons-edit-profile': function(e) {
     Router.go('userProfile', { _id : Meteor.userId() });
   },
+  'click #login-buttons-view-history': function(e){
+    Router.go('history', { _id : Meteor.userId() });
+  }
 });
--- client/templates/accountMenu/userProfile/userProfile.js
+++ client/templates/accountMenu/userProfile/userProfile.js
@@ -32,15 +32,18 @@ Template.userProfile.events({
       firstname : $(e.target).find('[name=firstname]').val(),
       lastname : $(e.target).find('[name=lastname]').val(),
       age : $(e.target).find('[name=age]').val(),
-      location : $(e.target).find('[name=location]').val()
+      location : $(e.target).find('[name=location]').val(),
+      description : $(e.target).find('[name=description]').val()
     }
+    console.log($(e.target));
     console.log('data :', data);
 
-    Meteor.users.update({_id:Meteor.userId()}, {$set : { 
+    Meteor.users.update({_id:Meteor.userId()}, {$set : {
       "profile.displayName.first" : data.firstname,
       "profile.displayName.last" : data.lastname,
       "profile.age" : data.age,
-      "profile.location" : data.location
+      "profile.location" : data.location,
+      "profile.description" : data.description
     }});
     console.log('Form submitted!');
   }
