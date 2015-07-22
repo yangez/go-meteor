0d67f2f1d15ff67fd7f60501a825bb1ca097dbf9
MKS-students <voxlol@outlook.com>
2015-07-21 20:02:19 -0500

    Created basic userprofile page

--- client/templates/accountMenu/accountMenu.js
+++ client/templates/accountMenu/accountMenu.js
@@ -1,8 +1,5 @@
 Template._loginButtonsLoggedInDropdown.events({
-  'click #login-buttons-edit-profile': function(e) {
-    Router.go('userProfile', { _id : Meteor.userId() });
+  'click #login-buttons-view-profile': function(e) {
+    Router.go('userProfile', { username : Meteor.userId().username });
   },
-  'click #login-buttons-view-history': function(e){
-    Router.go('history', { _id : Meteor.userId() });
-  }
 });
--- client/templates/accountMenu/userProfile/userProfileField.js
+++
@@ -1,5 +0,0 @@
-Template.profileField.helpers({
-  'property' : function(){
-    return JSON.stringify(this);
-  }
-})
