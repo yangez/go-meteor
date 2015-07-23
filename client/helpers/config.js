394a70ecafb891fa25b0406bb2621d1ef2e57b71
Eric Yang <e@eric-y.com>
2015-07-23 13:41:05 -0500

    Replaced client messages with popup message on top right

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -3,3 +3,10 @@ Accounts.ui.config({
 });
 
 notify.config({pageVisibility: true, autoClose: 2500})
+
+// local collection to display errors
+Errors = new Mongo.Collection(null);
+
+showMessage = function(message, type) {
+  Errors.insert({message: message});
+};
