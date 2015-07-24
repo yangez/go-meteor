c76c3a681772b3bd281305ec3cbc889b0b326457
Allen Chang <voxlol@outlook.com>
2015-07-24 01:04:05 -0500

    Implemented chat room. Hooked up event handlers. Added naive autoscroll

--- client/templates/globalChat/globalChat.js
+++ client/templates/globalChat/globalChat.js
@@ -1,7 +1,31 @@
-Template.globalChat.helpers({
+Template.globalChat.onRendered(function(){
+	$('.chat-messages').animate({scrollTop : 500});
 
 })
+
+Template.globalChat.helpers({
+	messages : function(){
+		$('.chat-messages').animate({scrollTop : 500});
+		return this.messages;
+	}
+});
 
 Template.globalChat.events({
-  
-})
+	'submit #chat-form-submit' : function(e){
+		e.preventDefault();
+		if(!Meteor.user()){
+			$("#enter-message-text").val('Please sign in at the top right corner. Thank you!');
+			return;
+		}
+		var globalChatId = Chatrooms.find({name: 'Global'})._id;
+		var timestamp = moment().format("hh:mm");
+		var text = $("#enter-message-text").val();
+		var msgData = {
+			user : Meteor.user().username,
+			time : timestamp,
+			text : text.trim()
+		}
+		Meteor.call('postToGlobalChat', msgData);
+		$("#enter-message-text").val('');
+	}
+});
