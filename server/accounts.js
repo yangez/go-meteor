13111799b3bb1ccc7820725b82a55068979b769f
MKS-students <voxlol@outlook.com>
2015-07-25 16:18:21 -0500

    Implemented max username length to 16 characters. Implemented case-insensitive username validation on signup

--- server/accounts.js
+++ server/accounts.js
@@ -1,3 +1,9 @@
+var accountConfig = {
+  maxLength : 16,
+  minLength : 3,
+}
+Object.freeze(accountConfig);
+
 Accounts.onCreateUser(function(options, user) {
   if (options.profile){
     user.profile = options.profile;
@@ -10,4 +16,28 @@ Accounts.onCreateUser(function(options, user) {
     user.profile.description = "";
   }
   return user;
+});
+
+Accounts.validateNewUser(function(user){
+  var passedLength = false;
+  var passedUsernameValidation = false;
+
+  if(user && user.username.length >= accountConfig.minLength && user.username.length <= accountConfig.maxLength){
+    passedLength = true;
+  }
+
+  if(user){
+    var allUsers = Meteor.users.find({}).fetch();
+    passedUsernameValidation = allUsers.every(function(userInArr){
+      return userInArr.username.toLowerCase() !== user.username.toLowerCase();
+    });
+  }
+
+  if(!passedLength)
+    throw new Meteor.Error('Invalid username length', 'Username must be between 3 and 16 characters.');
+  if(!passedUsernameValidation)
+    throw new Meteor.Error('Username is taken', 'That username is taken. Please enter a different username.');
+
+  if(passedLength && passedUsernameValidation) return true;
+  else return false;
 });
