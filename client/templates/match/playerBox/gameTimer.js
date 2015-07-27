009e6035d309873f5b022b41a1f8c788fd81e2ba
Eric Yang <e@eric-y.com>
2015-07-27 10:53:08 -0500

    Refactored byoyomi display on client side, counts down correctly now

--- client/templates/match/playerBox/gameTimer.js
+++ client/templates/match/playerBox/gameTimer.js
@@ -8,21 +8,40 @@ Template.gameTimer.helpers({
     var position = this.position;
     var color = game.getColorOfPosition(position);
 
-    var timeRemaining = game.absTimeRemaining(color);
+    var totalTimeLeft = game.totalTimeLeft(color);
+    var absTimeLeft = game.absTimeLeft(color);
+    var byoTimeLeft = game.byoTimeLeft(color);
+    
+    if (totalTimeLeft >= 0) {
+      var byoFormatted = game.byoFormatTimeLeft(color);
 
-    if (timeRemaining > 0 || timeRemaining === 0) {
-
-      game.checkTimerFlag();
-      return timeDisplay(timeRemaining);
+      return {
+        absolute: timeDisplay(absTimeLeft),
+        byoyomi: byoFormatted
+      }
 
     } else return false;
-  },
-  byoFormattedTime: function() {
-    var game = Games.findOne(this.game._id);
 
-    var position = this.position;
-    var color = game.getColorOfPosition(position);
 
-    return game.byoFormatTimeRemaining(color);
+    /*old
+    var absTimeRemaining = game.absTimeRemaining(color);
+    var byoTimeRemaining = game.byoTimeRemaining(color);
+
+    var totalTimeRemaining = absTimeRemaining + byoTimeRemaining;
+
+
+    if (totalTimeRemaining > 0 || totalTimeRemaining === 0) {
+
+      game.checkTimerFlag();
+
+      var byoFormatted = game.byoFormatTimeRemaining(color);
+
+      return {
+        absolute: timeDisplay(absTimeRemaining),
+        byoyomi: byoFormatted,
+      }
+
+    } else return false;
+    */
   }
 });
