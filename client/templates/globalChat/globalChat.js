b84aaf75363f87c581844b41e9a3a66d9c1e8d60
Jordan Chen <theevenprime2@yahoo.com>
2015-07-29 13:02:07 -0500

    prevent blank chat messages; trim all messages

--- client/templates/globalChat/globalChat.js
+++ client/templates/globalChat/globalChat.js
@@ -23,15 +23,19 @@ Template.globalChat.events({
 			$("#enter-message-text").val('Please sign in at the top right corner. Thank you!');
 			return;
 		}
+
 		var globalChatId = Chatrooms.find({name: 'Global'})._id;
 		var timestamp = moment().format("hh:mm");
 		var text = $("#enter-message-text").val();
-		var msgData = {
-			user : Meteor.user().username,
-			time : timestamp,
-			text : text.trim()
+
+		if (text.trim().length) {
+			var msgData = {
+				user : Meteor.user().username,
+				time : timestamp,
+				text : text.trim()
+			}
+			Meteor.call('postToGlobalChat', msgData);
+			$("#enter-message-text").val('');
 		}
-		Meteor.call('postToGlobalChat', msgData);
-		$("#enter-message-text").val('');
 	}
 });
