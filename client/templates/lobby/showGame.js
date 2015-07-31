823ce1a58c2624b87138f860dd7742ecde6d485d
Eric Yang <e@eric-y.com>
2015-07-31 15:22:53 -0500

    Added latest game board to main lobby

--- client/templates/lobby/showGame.js
+++ client/templates/lobby/showGame.js
@@ -2,6 +2,8 @@ Template.showGame.events({
   'click #board': function(e) {
     e.preventDefault(); e.stopPropagation();
     Router.go("match", {_id: this._id});
+  },
+});
 
-  }
+Template.showGame.helpers({
 });
