eab69337d22f6a0b35652b11e5e6051b6199059b
Eric Yang <e@eric-y.com>
2015-08-04 10:39:53 -0500

    Removed current game from 'Your Games' notification count

--- client/templates/menu/yourGamesMenu/yourGamesMenu.js
+++ client/templates/menu/yourGamesMenu/yourGamesMenu.js
@@ -1,39 +1,45 @@
 Template.yourGamesMenu.helpers({
   yourTurnCount: function() {
 
-    var games = Games.find({ $and: [
-        {archived: {$exists: false}}, // not archived
+    var and = [
+      {archived: {$exists: false}}, // not archived
 
-        // game has no open slot
-        {blackPlayerId: {$exists: true}},
-        {whitePlayerId: {$exists: true}},
+      // game has no open slot
+      {blackPlayerId: {$exists: true}},
+      {whitePlayerId: {$exists: true}},
 
-        // player isn't in MD and already accepted
-        {userAcceptedMD: {$ne: Meteor.userId() }},
+      // player isn't in MD and already accepted
+      {userAcceptedMD: {$ne: Meteor.userId() }},
 
+      {$or: [
+        // player is in MD and hasn't accepted
+        {$and: [
+          {markedDead: true},
+          {markedSchema: {$exists: true} },
+        ]},
+
+        // it's the player's move (and they're in-game)
         {$or: [
-          // player is in MD and hasn't accepted
-          {$and: [
-            {markedDead: true},
-            {markedSchema: {$exists: true} },
+          {$and:[
+            {blackPlayerId: Meteor.userId()},
+            {"wgoGame.turn": 1 }
           ]},
-
-          // it's the player's move (and they're in-game)
-          {$or: [
-            {$and:[
-              {blackPlayerId: Meteor.userId()},
-              {"wgoGame.turn": 1 }
-            ]},
-            {$and:[
-              {whitePlayerId: Meteor.userId()},
-              {"wgoGame.turn": -1 }
-            ]},
+          {$and:[
+            {whitePlayerId: Meteor.userId()},
+            {"wgoGame.turn": -1 }
           ]},
+        ]},
 
-        ]}
-      ]
-    });
+      ]}
+    ];
 
+    var currentRouteName = Router.current().route.getName();
+    if (currentRouteName === "match") {
+      var notCurrentGame = { _id: { $ne: Router.current().params._id } };
+      and.push(notCurrentGame);
+    }
+
+    var games = Games.find({ $and: and });
     return games.count();
 
 
