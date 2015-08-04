619806d236ac5d7326cd3c7d035a28ec1b52f65a
Jordan Chen <theevenprime2@yahoo.com>
2015-08-03 23:01:17 -0500

    non-reactive T_T

--- client/templates/match/spectators/spectatorContainer.js
+++ client/templates/match/spectators/spectatorContainer.js
@@ -1,10 +1,17 @@
 Template.spectatorContainer.onRendered(function(){
+  // var popoverTemplate = ['<div class="spectators-container popover">',
+  //                               '<div class="arrow"></div>',
+  //                               '<div class="popover-content">',
+  //                               '</div>',
+  //                           '</div>'].join('');
+
   $('.spectators-container').popover({
     placement: 'right',
     trigger: 'click',
     content: Blaze.toHTMLWithData(Template.spectator, {gameData: Template.currentData()}),
     container: 'body',
-    html: true
+    html: true,
+    // template: popoverTemplate
   });
 });
 
