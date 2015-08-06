bf598db5a2aa0181abb3e9d71c11ec8c0a5602e2
Eric Yang <e@eric-y.com>
2015-08-06 17:26:58 -0500

    Added challenge template items

---
+++ client/templates/menu/notificationsMenu/challenge/challengeDeclinedItem.js
@@ -0,0 +1,47 @@
+Template.challengeDeclinedItem.onRendered(function() {
+
+  var notification = this.data;
+
+  this.autorun(function() {
+
+    // mark game notifications as read if we're looking at the game
+    var routeName = Router.current().route.getName();
+    if (routeName === "match") {
+
+      var currentGameId = Router.current().params._id;
+      if (currentGameId === notification.data.gameId) {
+        var user = Meteor.user();
+        user.readNotification(notification._id);
+      }
+    }
+
+  });
+
+});
+
+Template.challengeDeclinedItem.helpers({
+  game: function() {
+    return Games.findOne(this.data.gameId);
+  },
+
+  // in game context
+  opponentColor: function() {
+    var thisColor = this.getColorOfPlayerId(Meteor.userId());
+    return getOppositeColor(thisColor);
+  },
+  color: function() {
+    return this.getColorOfPlayerId(Meteor.userId());
+  },
+});
+
+Template.challengeDeclinedItem.events({
+  'click .mark-read': function(e) {
+    e.stopPropagation();
+    var user = Meteor.user();
+    user.readNotification(this._id);
+  },
+  'click tr': function(e) {
+    Router.go("match", {_id: this.data.gameId});
+  },
+
+})
