55233e913d03b4c50c5fb1cc1b57052fb46e9808
Eric Yang <e@eric-y.com>
2015-08-07 19:59:39 -0500

    Removed some commenting

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -2,8 +2,6 @@ Accounts.ui.config({
   passwordSignupFields: 'USERNAME_AND_EMAIL'
 });
 
-// notify.config({pageVisibility: true, autoClose: 2500})
-
 // local collection to display errors
 Errors = new Mongo.Collection(null);
 
