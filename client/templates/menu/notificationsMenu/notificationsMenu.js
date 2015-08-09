6758280c555905e6033e9e5a4fe9334f1f9b6af4
Eric Yang <e@eric-y.com>
2015-08-09 17:31:01 -0500

    Created url parameter 'challenge=open' that opens challenge menu

--- client/templates/menu/notificationsMenu/notificationsMenu.js
+++ client/templates/menu/notificationsMenu/notificationsMenu.js
@@ -1,3 +1,12 @@
+Template.notificationsMenu.onRendered(function(){
+  this.autorun(function() {
+    if (Session.get("challengeMenuOpen")) {
+      $("#notifications-menu").dropdown("toggle");
+      Session.set("challengeMenuOpen", undefined);
+    }
+  })
+});
+
 Template.notificationsMenu.helpers({
   notifications: function() {
     return Herald.getNotifications({medium: 'onsite'});
